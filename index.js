3205a31f397d609e4c88e7c949e5cde633494864
sadrisso <sadriisso304@gmail.com>
2025-02-21 11:15:24 +0600

    get specific data by id

--- index.js
+++ index.js
@@ -101,6 +101,13 @@ async function run() {
             res.send(result)
         })
 
+        app.get("/task/:id", async (req, res) => {
+            const id = req.params.id
+            const query = {_id: new ObjectId(id)}
+            const result = await taskCollection.findOne(query)
+            res.send(result)
+        })
+
 
 
         // Send a ping to confirm a successful connection
